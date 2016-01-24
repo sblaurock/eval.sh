d1f77f5dbe9f1231b94aebee6ad87e0c9a1ed075
Steve Blaurock <sblaurock@shutterstock.com>
2016-01-23 23:32:52 -0500

    Set up views structure and index template.

--- index.js
+++ index.js
@@ -1,21 +1,32 @@
+// Initialize New Relic
+require('newrelic');
+
 // Application configuration
 var options = {
   port: 8080
 };
 
 // Dependencies
-var app = require('express')();
+var express = require('express');
+var app = express();
 var bodyParser = require('body-parser');
-var system = require("./utils/system");
-var newrelic = require('newrelic');
+var system = require('./utils/system');
+var nunjucks = require('nunjucks');
 
 system.guardUID();
 app.use(bodyParser.json());
-
+app.use(express.static(__dirname + '/public'));
 app.set('port', (process.env.PORT || options.port));
 
+// Setup Nunjucks
+nunjucks.configure('views', {
+  autoescape: true,
+  express: app,
+  watch: true
+});
+
 app.get('/', function(req, res) {
-  res.sendStatus(200);
+  res.render('index.html');
 });
 
 app.listen(app.get('port'), function() {
